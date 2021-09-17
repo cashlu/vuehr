c80e38237cd18becd7860e3595488d44435ab644
cashlu <cashlu@yeah.net>
2021-09-17 21:59:54 +0800

    初始化项目

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+
+        <el-form :roles="roles" :model="loginForm">
+        </el-form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            roles: {
+                username: [{required: true, message: "请输入用户名", trigger: "blur"}],
+                password:[{required: true, message: "请输入密码", trigger: "blur"}]
+            },
+            loginForm:{}
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
