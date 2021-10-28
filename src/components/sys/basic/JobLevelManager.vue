58b7b3cf3d1ed3710e4a887d78a1fe1b960bd96b
cashlu <cashlu@yeah.net>
2021-10-29 03:37:15 +0800

    添加了职称管理的页面

--- src/components/sys/basic/JobLevelManager.vue
+++ src/components/sys/basic/JobLevelManager.vue
@@ -1,15 +1,120 @@
 <template>
-<div>
-    职称管理
-</div>
+    <div>
+        <div>
+            <el-input class="jobLevelNameInput"
+                      prefix-icon="el-icon-plus"
+                      size="small"
+                      placeholder="添加职称..."
+                      v-model="jobLevel.name">
+            </el-input>
+            <el-select v-model="jobLevel.titleLevel" placeholder="职称等级" size="small">
+                <el-option
+                    v-for="item in titleLevels"
+                    :key="item"
+                    :label="item"
+                    :value="item">
+                </el-option>
+            </el-select>
+            <el-button class="addJobLevelBtn"
+                       icon="el-icon-plus"
+                       size="small"
+                       type="primary">添加
+            </el-button>
+        </div>
+        <div class="tableDiv">
+            <el-table
+                :data="jobLevels"
+                @selection-change="handleSelectionChange"
+                border
+                stripe
+                size="small"
+                style="width: 70%">
+                <el-table-column type="selection"
+                                 width="55">
+                </el-table-column>
+                <el-table-column prop="id"
+                                 label="编号"
+                                 width="55">
+                </el-table-column>
+                <el-table-column prop="name"
+                                 label="职称名称"
+                                 width="180">
+                </el-table-column>
+                <el-table-column prop="titleLevel"
+                                 label="职称等级"
+                                 width="180">
+                </el-table-column>
+                <el-table-column prop="createDate"
+                                 label="创建时间"
+                                 width="180">
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                        <el-button
+                            size="mini"
+                            @click="showEditDialog(scope.row)">编辑
+                        </el-button>
+                        <el-button
+                            size="mini"
+                            type="danger"
+                            @click="handleDelete(scope.row)">删除
+                        </el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "JobLevelManager"
+    name: "JobLevelManager",
+    data() {
+        return {
+            jobLevel: {
+                name: "",
+                titleLevel: "",
+            },
+            // 这个属性最理想还是应该由后端来维护
+            titleLevels: ["正高级", "副高级", "中级", "初级", "员级"],
+            jobLevels: [],
+        }
+    },
+    methods: {
+        getAllJobLevels(){
+
+        },
+        addJobLevel() {
+
+        },
+        handleSelectionChange() {
+
+        },
+        showEditDialog(row) {
+
+        },
+        handleDelete(row) {
+
+        }
+    },
+    mounted() {
+        this.getAllJobLevels()
+    }
 }
 </script>
 
-<style scoped>
+<style>
+.jobLevelNameInput {
+    width: 300px;
+    margin-right: 10px;
+}
 
+.addJobLevelBtn {
+    margin-left: 10px;
+}
+
+.tableDiv{
+    margin-top: 10px;
+}
 </style>
--- src/views/sys/SysBasic.vue
+++ src/views/sys/SysBasic.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <el-tabs v-model="activeName" type="card">
-            <el-tab-pane label="部门管理" name="first">
+            <el-tab-pane label="部门管理" name="DepManager">
                 <DepManager></DepManager>
             </el-tab-pane>
-            <el-tab-pane label="职位管理" name="second">
+            <el-tab-pane label="职位管理" name="PosManager">
                 <PosManager></PosManager>
             </el-tab-pane>
-            <el-tab-pane label="职称管理" name="third">
+            <el-tab-pane label="职称管理" name="JobLevelManager">
                 <JobLevelManager></JobLevelManager>
             </el-tab-pane>
-            <el-tab-pane label="奖惩规则" name="fourth">
+            <el-tab-pane label="奖惩规则" name="EcManager">
                 <EcManager></EcManager>
             </el-tab-pane>
-            <el-tab-pane label="权限组" name="fifth">
+            <el-tab-pane label="权限组" name="PermissionManager">
                 <PermissionManager></PermissionManager>
             </el-tab-pane>
 
